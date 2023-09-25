988dd2b3cf57e72d11d175a0873e181eb1bad07a
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-09-25 13:10:20 +0200

    Add files via upload

---
+++ opakovani7/opakovani7/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace opakovani7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(97, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(43, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "n  = ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(210, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Spočti";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(34, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(34, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(34, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 310);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ opakovani7/opakovani7/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opakovani7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.Text = "ukol 7";
+            this.ShowIcon = false;
+            this.BackColor = Color.DodgerBlue;
+            this.ForeColor = Color.White;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            StreamReader sr = null;
+            try
+            {
+                int n = int.Parse(textBox1.Text);
+                sr = new StreamReader("cisla.txt", Encoding.GetEncoding("Windows-1250"));
+
+                int iterace = 1;
+                long cislo = 0;
+
+                while (!sr.EndOfStream)
+                {
+                    if (iterace == 5)
+                    {
+                        cislo = Convert.ToInt64(sr.ReadLine());
+                        break;
+                    }
+                    else sr.ReadLine();
+                    iterace++;
+                }
+
+
+                label2.Text = "Mocnina: "+cislo + "^" + n + " = ";
+                
+               
+
+                if (n == 0) label2.Text += "1";
+                else if (n < 0)
+                {
+                    n = -n;
+                    double vysledek = 1;
+                    for (int i = 0; i < n; i++)
+                    {
+                        checked { vysledek /= (double)cislo; }
+                    }
+                    label2.Text += vysledek.ToString();
+                    n = -n;
+                }
+                else
+                {
+                    long vysledek = cislo;
+                    for (int i = 1; i < n; i++)
+                    {
+                        checked { vysledek *= cislo; }
+                    }
+                    label2.Text += vysledek.ToString();
+                }
+
+                label3.Text = "Realny: " + cislo + "/" + n + " = ";
+                label4.Text = "Celociselny: " + cislo + "/" + n + " = ";
+                checked
+                {
+                    double realny = Math.Round((double)cislo / (double)n,8);
+                    int celo = (int)cislo / n;
+                    label3.Text += realny.ToString();
+                    label4.Text += celo.ToString();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                MessageBox.Show(ex.Message);           
+            }
+            catch (DivideByZeroException ex)
+            {
+                label3.Text = "";
+                label4.Text = "";
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
